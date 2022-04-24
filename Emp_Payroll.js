f10204670a7d5946d3a85cdde823974d61ff8d46
prakashudikeri <pu010889@gmail.com>
2022-04-24 14:11:26 +0530

    Regex & catch Exception

--- Emp_Payroll.js
+++ Emp_Payroll.js
@@ -12,7 +12,11 @@ class EmployeePayrollData{
     }
     //getter & setter method
     get name () {return this._name;}
-    set name (name) {this._name = name;}
+    set name (name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+            if (nameRegex.test(name)) this._name = name;
+            else throw 'Name is Incorrect!';
+    }
 
     //method
     toString() {
@@ -26,7 +30,9 @@ class EmployeePayrollData{
 
 let employeePayrollData = new EmployeePayrollData (1, "Mark", 303030);
     console.log(employeePayrollData.toString());
-employeePayrollData.name = "John";
+try{
+    employeePayrollData.name = "John";
     console.log(employeePayrollData.toString());
+} catch (e) { console.error(e);}
 let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
     console.log(newEmployeePayrollData.toString());
